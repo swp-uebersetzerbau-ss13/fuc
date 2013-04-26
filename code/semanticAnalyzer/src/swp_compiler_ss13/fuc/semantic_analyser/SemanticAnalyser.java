5ad21e0d70033e70a62705f3c24fb5d994417b5a
swpcomp-heydu <swpcomp-heydu@swp-uebersetzerbau-ss13>
2013-04-26 20:58:33 +0200

    [SemanticAnalyser] Temporary interface for parser
    The class SemanticAnalyser is meant to be used by the parser.

--- code/semanticAnalyzer/src/swp_compiler_ss13/fuc/semantic_analyser/SemanticAnalyser.java
+++ code/semanticAnalyzer/src/swp_compiler_ss13/fuc/semantic_analyser/SemanticAnalyser.java
@@ -1,9 +1,17 @@
 package swp_compiler_ss13.fuc.semantic_analyser;
 
 import swp_compiler_ss13.common.ast.AST;
+import swp_compiler_ss13.common.parser.ReportLog;
 
 public class SemanticAnalyser {
-    public AST analyse(AST ast) {
-	return ast;
-    }
+
+	private ReportLog _errorLog;
+
+	public SemanticAnalyser(ReportLog errorLog) {
+		_errorLog = errorLog;
+	}
+
+	public AST analyse(AST ast) {
+		return ast;
+	}
 }
--- code/semanticAnalyzer/test/swp_compiler_ss13/fuc/semantic_analyser/SemanticAnalyserTest.java
+++ code/semanticAnalyzer/test/swp_compiler_ss13/fuc/semantic_analyser/SemanticAnalyserTest.java
@@ -8,38 +8,39 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SemanticAnalyserTest {
-    
-    public SemanticAnalyserTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of analyse method, of class SemanticAnalyser.
-     */
-    @Test
-    public void testAnalyse() {
-        System.out.println("analyse");
-        swp_compiler_ss13.common.ast.AST ast = null;
-        SemanticAnalyser instance = new SemanticAnalyser();
-        swp_compiler_ss13.common.ast.AST expResult = null;
-        swp_compiler_ss13.common.ast.AST result = instance.analyse(ast);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+	public SemanticAnalyserTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() {
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Test of analyse method, of class SemanticAnalyser.
+	 */
+	@Test
+	public void testAnalyse() {
+		System.out.println("analyse");
+		swp_compiler_ss13.common.ast.AST ast = null;
+		swp_compiler_ss13.common.parser.ReportLog log = null;
+		SemanticAnalyser instance = new SemanticAnalyser(log);
+		swp_compiler_ss13.common.ast.AST expResult = null;
+		swp_compiler_ss13.common.ast.AST result = instance.analyse(ast);
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
 }
