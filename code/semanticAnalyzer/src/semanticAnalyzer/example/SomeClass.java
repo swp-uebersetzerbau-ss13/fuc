924e9a082a3b428743b3e0e3972a2ef3d88322a0
swpcomp-heydu <swpcomp-heydu@swp-uebersetzerbau-ss13>
2013-04-26 20:46:42 +0200

    [SemanticAnalyser] Added a stub class

--- code/semanticAnalyzer/src/semanticAnalyzer/example/SomeClass.java
+++
@@ -1,5 +0,0 @@
-package semanticAnalyzer.example;
-
-public class SomeClass {
-
-}
---
+++ code/semanticAnalyzer/src/swp_compiler_ss13/fuc/semantic_analyser/SemanticAnalyser.java
@@ -0,0 +1,9 @@
+package swp_compiler_ss13.fuc.semantic_analyser;
+
+import swp_compiler_ss13.common.ast.AST;
+
+public class SemanticAnalyser {
+    public AST analyse(AST ast) {
+	return ast;
+    }
+}
