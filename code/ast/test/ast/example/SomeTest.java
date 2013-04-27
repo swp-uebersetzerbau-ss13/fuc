587dd4bf05d58f9300f3c47c1e6f2bb465ab9f1b
SWP-Comp-Ch3ck3r <frank.zechert@fu-berlin.de>
2013-04-27 03:02:50 +0200

    modified build scripts

--- code/ast/test/ast/example/SomeTest.java
+++ code/ast/test/ast/example/SomeTest.java
@@ -1,7 +1,5 @@
 package ast.example;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -28,7 +26,7 @@ public class SomeTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 }
