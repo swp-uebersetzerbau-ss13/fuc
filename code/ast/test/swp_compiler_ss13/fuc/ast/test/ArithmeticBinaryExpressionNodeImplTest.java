8fcd2ca3b4e001bf8f44387d4e639bb7771f5c7f
SWP-Comp-Ch3ck3r <frank.zechert@fu-berlin.de>
2013-04-27 17:12:33 +0200

    added tests

--- code/ast/test/swp_compiler_ss13/fuc/ast/test/ArithmeticBinaryExpressionNodeImplTest.java
+++ code/ast/test/swp_compiler_ss13/fuc/ast/test/ArithmeticBinaryExpressionNodeImplTest.java
@@ -1,10 +1,18 @@
 package swp_compiler_ss13.fuc.ast.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
+import java.util.List;
+
+import junit.extensions.PA;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import swp_compiler_ss13.common.ast.ASTNode;
 import swp_compiler_ss13.common.ast.ASTNode.ASTNodeType;
+import swp_compiler_ss13.common.ast.nodes.ExpressionNode;
 import swp_compiler_ss13.common.ast.nodes.binary.ArithmeticBinaryExpressionNode;
 import swp_compiler_ss13.fuc.ast.ArithmeticBinaryExpressionNodeImpl;
 
@@ -16,14 +24,85 @@ import swp_compiler_ss13.fuc.ast.ArithmeticBinaryExpressionNodeImpl;
  */
 public class ArithmeticBinaryExpressionNodeImplTest {
 
+	private ArithmeticBinaryExpressionNode node;
+
+	/**
+	 * Set up test environment
+	 * 
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		this.node = new ArithmeticBinaryExpressionNodeImpl();
+	}
+
 	/**
 	 * Test if the node returns the correct node type
 	 */
 	@Test
 	public void testGetNodeType() {
-		ArithmeticBinaryExpressionNode node = new ArithmeticBinaryExpressionNodeImpl();
-		ASTNodeType nodeType = node.getNodeType();
+		ASTNodeType nodeType = this.node.getNodeType();
 		assertEquals(ASTNodeType.ArithmeticBinaryExpressionNode, nodeType);
 	}
 
+	/**
+	 * Test the returned children when left and right expressions are empty
+	 */
+	@Test
+	public void testGetChildrenNone() {
+		List<ASTNode> children = this.node.getChildren();
+		assertEquals(0, children.size());
+	}
+
+	/**
+	 * Test the returned children when left expression is empty
+	 */
+	@Test
+	public void testGetChildrenRight() {
+		this.node.setRightValue(new ArithmeticBinaryExpressionNodeImpl());
+		List<ASTNode> children = this.node.getChildren();
+		assertEquals(1, children.size());
+	}
+
+	/**
+	 * Test the returned children when right expression is empty
+	 */
+	@Test
+	public void testGetChildrenLeft() {
+		this.node.setLeftValue(new ArithmeticBinaryExpressionNodeImpl());
+		List<ASTNode> children = this.node.getChildren();
+		assertEquals(1, children.size());
+	}
+
+	/**
+	 * Test the returned children when right and left expressions are non empty
+	 */
+	@Test
+	public void testGetChildrenBoth() {
+		this.node.setLeftValue(new ArithmeticBinaryExpressionNodeImpl());
+		this.node.setRightValue(new ArithmeticBinaryExpressionNodeImpl());
+		List<ASTNode> children = this.node.getChildren();
+		assertEquals(2, children.size());
+	}
+
+	/**
+	 * Test the getLeftValue method
+	 */
+	@Test
+	public void testGetLeftValue() {
+		ExpressionNode leftTestValue = new ArithmeticBinaryExpressionNodeImpl();
+		PA.setValue(this.node, "leftExpression", leftTestValue);
+
+		ExpressionNode actualValue = this.node.getLeftValue();
+		assertSame(leftTestValue, actualValue);
+	}
+
+	/**
+	 * Test the getLeftValue method for null
+	 */
+	@Test
+	public void testGetLeftValueNull() {
+		ExpressionNode actualValue = this.node.getLeftValue();
+		assertEquals(null, actualValue);
+	}
 }
