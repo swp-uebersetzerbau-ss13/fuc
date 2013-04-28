814c42618e4c1c2375ab284c91dc6f63b153e25e
SWP-Comp-Ch3ck3r <frank.zechert@fu-berlin.de>
2013-04-28 21:11:36 +0200

    added DFSLTRIterator

--- code/ast/src/swp_compiler_ss13/fuc/ast/ASTNodeImpl.java
+++ code/ast/src/swp_compiler_ss13/fuc/ast/ASTNodeImpl.java
@@ -1,6 +1,8 @@
 package swp_compiler_ss13.fuc.ast;
 
 import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import swp_compiler_ss13.common.ast.ASTNode;
 
@@ -29,30 +31,47 @@ public abstract class ASTNodeImpl implements ASTNode {
 
 	@Override
 	public Iterator<ASTNode> getDFSLTRNodeIterator() {
-		// FIXME rework this method, it does not seem to work correct
-		throw new UnsupportedOperationException();
-		/*
-		 * final List<ASTNode> children = this.getChildren(); final
-		 * Iterator<ASTNode> childIterator = children.iterator();
-		 * Iterator<ASTNode> iterator = new Iterator<ASTNode>() {
-		 * 
-		 * Iterator<ASTNode> currentNodeIterator; Boolean outputNode = false;
-		 * ASTNode nextChild;
-		 * 
-		 * @Override public void remove() { throw new
-		 * UnsupportedOperationException(); }
-		 * 
-		 * @Override public ASTNode next() { if (this.outputNode) {
-		 * this.outputNode = false; return this.nextChild; } if
-		 * (!this.hasNext()) { throw new NoSuchElementException(); } return
-		 * this.currentNodeIterator.next(); }
-		 * 
-		 * @Override public boolean hasNext() { if (this.currentNodeIterator !=
-		 * null) { if (this.currentNodeIterator.hasNext()) { return true; } } if
-		 * (childIterator.hasNext()) { this.nextChild = childIterator.next();
-		 * this.currentNodeIterator = this.nextChild.getDFSLTRNodeIterator();
-		 * this.outputNode = true; return true; } return false; } }; return
-		 * iterator;
-		 */
+		final List<ASTNode> children = this.getChildren();
+		final Iterator<ASTNode> childIterator = children.iterator();
+		Iterator<ASTNode> iterator = new Iterator<ASTNode>() {
+
+			Iterator<ASTNode> currentNodeIterator;
+			Boolean outputNode = false;
+			ASTNode nextChild;
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public ASTNode next() {
+				if (this.outputNode) {
+					this.outputNode = false;
+					return this.nextChild;
+				}
+				if (!this.hasNext()) {
+					throw new NoSuchElementException();
+				}
+				return this.currentNodeIterator.next();
+			}
+
+			@Override
+			public boolean hasNext() {
+				if (this.currentNodeIterator != null) {
+					if (this.currentNodeIterator.hasNext()) {
+						return true;
+					}
+				}
+				if (childIterator.hasNext()) {
+					this.nextChild = childIterator.next();
+					this.currentNodeIterator = this.nextChild.getDFSLTRNodeIterator();
+					this.outputNode = true;
+					return true;
+				}
+				return false;
+			}
+		};
+		return iterator;
 	}
 }
