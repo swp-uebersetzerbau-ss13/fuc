51fe02906220dffb9b582a92a83b8e5c558a3c2a
EdwarDDay <the_brainman@web.de>
2013-04-30 13:24:08 +0200

    1st version of semantic analyzer

--- code/semanticAnalyzer/src/swp_compiler_ss13/fuc/semantic_analyser/SemanticAnalyser.java
+++ code/semanticAnalyzer/src/swp_compiler_ss13/fuc/semantic_analyser/SemanticAnalyser.java
@@ -1,17 +1,38 @@
 package swp_compiler_ss13.fuc.semantic_analyser;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import swp_compiler_ss13.common.ast.AST;
+import swp_compiler_ss13.common.ast.ASTNode;
 import swp_compiler_ss13.common.parser.ReportLog;
 
 public class SemanticAnalyser {
 
+	enum Attribute {
+		/**
+		 * num, basic, array,...
+		 */
+		TYPE
+	}
+
 	private ReportLog _errorLog;
+	private Map<ASTNode, Map<Attribute, String>> attributes;
 
 	public SemanticAnalyser(ReportLog errorLog) {
+		attributes = new HashMap<>();
 		_errorLog = errorLog;
 	}
 
 	public AST analyse(AST ast) {
 		return ast;
+	}
+
+	private String getAttribute(ASTNode node, Attribute attribute) {
+		return attributes.get(node).get(attribute);
+	}
+
+	private void setAttribute(ASTNode node, Attribute attribute, String value) {
+		attributes.get(node).put(attribute, value);
 	}
 }
