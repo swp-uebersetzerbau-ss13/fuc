87bdada1cdd195db0fd350d07801563a3287d232
Moritz Maxeiner <moritz@ucworks.org>
2013-04-30 16:53:35 +0200

    Add some more tests

--- code/backend/test/swp_compiler_ss13/fuc/backend/LLVMBackendTest.java
+++ code/backend/test/swp_compiler_ss13/fuc/backend/LLVMBackendTest.java
@@ -41,21 +41,169 @@ public class LLVMBackendTest {
     public void tearDown() throws Exception {
     }
 
+	@Test
+    public void generateTargetCodeTest_DeclareLong() throws IOException {
+        Quadruple declaration = new Q(
+	        Quadruple.Operator.DECLARE_LONG,
+	        Quadruple.EmptyArgument,
+	        Quadruple.EmptyArgument,
+	        "longVariable");
+        InputStream codeStream = generateCode(declaration);
+        String generatedCode = buildString(codeStream);
+        String expectedCode = "define i64 @main() {\n  %longVariable = alloca i64\n  ret i64 0\n}\n";
+        assertEquals(expectedCode, generatedCode);
+    }
 
-    @Test
-    public void generateTargetCodeDeclarationTest() throws IOException {
-        QuadrupleImpl declaration = new QuadrupleImpl(Quadruple.Operator.DECLARE_BOOLEAN, Quadruple.EmptyArgument, Quadruple.EmptyArgument, "boolVariable");
+	@Test
+    public void generateTargetCodeTest_DeclareLong_InitConst() throws IOException {
+        Quadruple declaration = new Q(
+	        Quadruple.Operator.DECLARE_LONG,
+	        "#0",
+	        Quadruple.EmptyArgument,
+	        "longVariable");
+        InputStream codeStream = generateCode(declaration);
+        String generatedCode = buildString(codeStream);
+        String expectedCode = "define i64 @main() {\n"
+	        + "  %longVariable = alloca i64\n"
+	        + "  store i64 0, i64* %longVariable\n"
+	        + "  ret i64 0\n}\n";
+        assertEquals(expectedCode, generatedCode);
+    }
+
+	@Test
+    public void generateTargetCodeTest_DeclareLong_InitVar() throws IOException {
+        ArrayList<Quadruple> tac = new ArrayList<Quadruple>();
+		tac.add(new Q(
+			        Quadruple.Operator.DECLARE_LONG,
+			        Quadruple.EmptyArgument,
+			        Quadruple.EmptyArgument,
+			        "init"));
+		tac.add(new Q(
+			        Quadruple.Operator.DECLARE_LONG,
+			        "init",
+			        Quadruple.EmptyArgument,
+			        "longVariable"));
+        InputStream codeStream = generateCode(tac);
+        String generatedCode = buildString(codeStream);
+        String expectedCode = "define i64 @main() {\n"
+	        + "  %init = alloca i64\n"
+	        + "  %longVariable = alloca i64\n"
+	        + "  %init.0 = load i64* %init\n"
+	        + "  store i64 %init.0, i64* %longVariable\n"
+	        + "  ret i64 0\n}\n";
+        assertEquals(expectedCode, generatedCode);
+    }
+
+	@Test
+    public void generateTargetCodeTest_DeclareDouble() throws IOException {
+        Quadruple declaration = new Q(
+	        Quadruple.Operator.DECLARE_DOUBLE,
+	        Quadruple.EmptyArgument,
+	        Quadruple.EmptyArgument,
+	        "doubleVariable");
+        InputStream codeStream = generateCode(declaration);
+        String generatedCode = buildString(codeStream);
+        String expectedCode = "define i64 @main() {\n  %doubleVariable = alloca double\n  ret i64 0\n}\n";
+        assertEquals(expectedCode, generatedCode);
+    }
+
+	@Test
+    public void generateTargetCodeTest_DeclareBoolean() throws IOException {
+        Quadruple declaration = new Q(
+	        Quadruple.Operator.DECLARE_BOOLEAN,
+	        Quadruple.EmptyArgument,
+	        Quadruple.EmptyArgument,
+	        "boolVariable");
         InputStream codeStream = generateCode(declaration);
         String generatedCode = buildString(codeStream);
         String expectedCode = "define i64 @main() {\n  %boolVariable = alloca i8\n  ret i64 0\n}\n";
         assertEquals(expectedCode, generatedCode);
     }
 
+	@Test
+    public void generateTargetCodeTest_DeclareString() throws IOException {
+        Quadruple declaration = new Q(
+	        Quadruple.Operator.DECLARE_STRING,
+	        Quadruple.EmptyArgument,
+	        Quadruple.EmptyArgument,
+	        "stringVariable");
+        InputStream codeStream = generateCode(declaration);
+        String generatedCode = buildString(codeStream);
+        String expectedCode = "define i64 @main() {\n  %stringVariable = alloca i8*\n  ret i64 0\n}\n";
+        assertEquals(expectedCode, generatedCode);
+    }
+
+	@Test
+    public void generateTargetCodeTest_LongToDouble() throws IOException {
+		ArrayList<Quadruple> tac = new ArrayList<Quadruple>();
+		tac.add(new Q(
+			        Quadruple.Operator.DECLARE_LONG,
+			        Quadruple.EmptyArgument,
+			        Quadruple.EmptyArgument,
+			        "longVariable"));
+		tac.add(new Q(
+			        Quadruple.Operator.DECLARE_DOUBLE,
+			        Quadruple.EmptyArgument,
+			        Quadruple.EmptyArgument,
+			        "doubleVariable"));
+		tac.add(new Q(
+			        Quadruple.Operator.LONG_TO_DOUBLE,
+			        "longVariable",
+			        Quadruple.EmptyArgument,
+				        "doubleVariable"));
+        InputStream codeStream = generateCode(tac);
+        String generatedCode = buildString(codeStream);
+        String expectedCode = "define i64 @main() {\n"
+	        + "  %longVariable = alloca i64\n"
+	        + "  %doubleVariable = alloca double\n"
+	        + "  %longVariable.0 = load i64* %longVariable\n"
+	        + "  %doubleVariable.0 = sitofp i64 %longVariable.0 to double\n"
+	        + "  store double %doubleVariable.0, double* %doubleVariable\n"
+	        + "  ret i64 0\n}\n";
+        assertEquals(expectedCode, generatedCode);
+    }
+
+	@Test
+    public void generateTargetCodeTest_DoubleToLong() throws IOException {
+		ArrayList<Quadruple> tac = new ArrayList<Quadruple>();
+		tac.add(new Q(
+			        Quadruple.Operator.DECLARE_LONG,
+			        Quadruple.EmptyArgument,
+			        Quadruple.EmptyArgument,
+			        "longVariable"));
+		tac.add(new Q(
+			        Quadruple.Operator.DECLARE_DOUBLE,
+			        Quadruple.EmptyArgument,
+			        Quadruple.EmptyArgument,
+			        "doubleVariable"));
+		tac.add(new Q(
+			        Quadruple.Operator.DOUBLE_TO_LONG,
+			        "doubleVariable",
+			        Quadruple.EmptyArgument,
+				        "longVariable"));
+        InputStream codeStream = generateCode(tac);
+        String generatedCode = buildString(codeStream);
+        String expectedCode = "define i64 @main() {\n"
+	        + "  %longVariable = alloca i64\n"
+	        + "  %doubleVariable = alloca double\n"
+	        + "  %doubleVariable.0 = load double* %doubleVariable\n"
+	        + "  %longVariable.0 = fptosi double %doubleVariable.0 to i64\n"
+	        + "  store i64 %longVariable.0, i64* %longVariable\n"
+	        + "  ret i64 0\n}\n";
+        assertEquals(expectedCode, generatedCode);
+    }
+
 
     private InputStream generateCode(Quadruple quadruple) {
-        ArrayList<Quadruple> tacList = new ArrayList<>();
+        ArrayList<Quadruple> tacList = new ArrayList<Quadruple>();
         tacList.add(quadruple);
         Map<String, InputStream> result = backend.generateTargetCode(tacList);
+        InputStream module = result.get(".ll");
+        return module;
+    }
+
+	private InputStream generateCode(ArrayList<Quadruple> tac) {
+        Map<String, InputStream> result = backend.generateTargetCode(tac);
         InputStream module = result.get(".ll");
         return module;
     }
@@ -68,4 +216,32 @@ public class LLVMBackendTest {
         return builder.toString();
     }
 
+	/**
+	 * A bare-bones implementation of the
+	 * <code>Quadruple</code> interface used to
+	 * generate the left hand side for several
+	 * of the above test cases.
+	 */
+	private static class Q implements Quadruple
+	{
+		private Operator operator;
+		private String argument1;
+		private String argument2;
+		private String result;
+
+		public Q(Operator o, String a1, String a2, String r)
+		{
+			operator = o;
+			argument1 = a1;
+			argument2 = a2;
+			result = r;
+		}
+
+		public String toString() { return "(" + String.valueOf(operator) + "|" + argument1  + "|" + argument2 + "|" + result + ")"; }
+
+		public Operator getOperator() { return operator; }
+		public String getArgument1() { return argument1; }
+		public String getArgument2() { return argument2; }
+		public String getResult() { return result; }
+	}
 }
