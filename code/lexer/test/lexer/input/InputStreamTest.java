c17a56036d3ace5256524ace2543141d9e4dd44c
Ckorvus <tay.ho@hotmail.com>
2013-05-01 19:35:06 +0300

    Update InputStreamTest.java
    edited

--- code/lexer/test/lexer/input/InputStreamTest.java
+++ code/lexer/test/lexer/input/InputStreamTest.java
@@ -1,14 +1,6 @@
-package lexer.input;
+package lexer.declaration;
 
-import static org.junit.Assert.*;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import lexer.LexerTestSuite;
-import swp_compiler_ss13.common.lexer.LexerClass;
-import swp_compiler_ss13.common.lexer.TokenClass;
+import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.Before;
@@ -16,39 +8,23 @@ import org.junit.Test;
 
 /**
  * 
- * @author Thomas Benndorf
+ * @author cruaier
  * @since 28.04.2013
  * 
  */
-public class InputStreamTest {
-	private InputStream stream;
-	private LexerClass lexer;
+public class DeclarationTest {
 
 	@Before
 	public void setUp() throws Exception {
-		this.stream = new FileInputStream(LexerTestSuite.fileLocation1);
-		this.lexer = new LexerClass();
-		this.lexer.setSourceStream(this.stream);
-
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		
 	}
 
 	@Test
-	public void setInputStreamCorrect() throws IOException {
-		assertTrue(this.lexer.is != null);
-
-		assertEquals(this.stream.read() != -1, this.lexer.is.read() != -1);
+	public void test() {
+		fail("Not yet implemented");
 	}
 
-	@Test
-	public void readTokenCorrect() {
-		
-		TokenClass token = this.lexer.getNextToken();
-		assertTrue(token != null);
-	}
-	
 }
