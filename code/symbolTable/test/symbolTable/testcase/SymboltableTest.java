16625e38be09be684d426a3480a563d85c3dc089
kensan <kensan@linuxmint.linuxmint-domain>
2013-05-02 00:10:24 +0200

    testcase and symboltable complete

--- code/symbolTable/test/symbolTable/testcase/SymboltableTest.java
+++ code/symbolTable/test/symbolTable/testcase/SymboltableTest.java
@@ -1,54 +1,87 @@
 package symbolTable.testcase;
 
-import static org.junit.Assert.fail;
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import swp_compiler_ss13.common.parser.SymbolTable;
+import swp_compiler_ss13.common.types.BooleanType;
+import swp_compiler_ss13.common.types.NumType;
+import swp_compiler_ss13.common.types.RealType;
 import symbolTable.impl.SymbolTableImpl;
 
 public class SymboltableTest {
 
 	
 	private SymbolTable symbolTable;
+	private SymbolTable parentTable;
 
 	@Before
 	public void setUp() throws Exception {
 		symbolTable = new SymbolTableImpl();
+		parentTable = new SymbolTableImpl(symbolTable);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * Test for parent symboltable
+	 */
 	@Test
 	public void testGetParentSymbolTable() {
-		fail("Not yet implemented");
+		Assert.assertNull(symbolTable.getParentSymbolTable());
+		Assert.assertEquals(parentTable.getParentSymbolTable(), symbolTable);
 	}
 
-	@Test
-	public void testIsDeclared() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testLookupType() {
-		fail("Not yet implemented");
+		NumType type = new NumType();
+		symbolTable.insert("arg1",type);
+		BooleanType type2 = new BooleanType();
+		symbolTable.insert("arg2", type2);
+		RealType type3 = new RealType();
+		symbolTable.insert("arg3", type3);
+		Assert.assertTrue(symbolTable.isDeclared("arg1"));
+		Assert.assertTrue(symbolTable.isDeclared("arg2"));
+		Assert.assertTrue(symbolTable.isDeclared("arg3"));
+		Assert.assertEquals(symbolTable.lookupType("arg1"), type);
+		Assert.assertEquals(symbolTable.lookupType("arg2"), type2);
+		Assert.assertEquals(symbolTable.lookupType("arg3"), type3);
+		Assert.assertNotSame(symbolTable.lookupType("arg1"), type2);
+		Assert.assertNull(symbolTable.lookupType("arg"));
 	}
 
 	@Test
 	public void testInsert() {
-		fail("Not yet implemented");
+		symbolTable.insert("arg1",new NumType());
+		symbolTable.insert("arg2", new BooleanType());
+		symbolTable.insert("arg3", new RealType());
+		Assert.assertTrue(symbolTable.isDeclared("arg1"));
+		Assert.assertTrue(symbolTable.isDeclared("arg2"));
+		Assert.assertTrue(symbolTable.isDeclared("arg3"));
+		Assert.assertFalse(symbolTable.isDeclared("arg"));
 	}
-
+	
+	
 	@Test
 	public void testRemove() {
-		fail("Not yet implemented");
+		symbolTable.insert("arg1",new NumType());
+		symbolTable.insert("arg2", new BooleanType());
+		Assert.assertTrue(symbolTable.isDeclared("arg1"));
+		Assert.assertTrue(symbolTable.isDeclared("arg2"));
+		Assert.assertTrue(symbolTable.remove("arg1"));
+		Assert.assertTrue(symbolTable.remove("arg2"));
+		Assert.assertFalse(symbolTable.remove("arg"));
+		Assert.assertFalse(symbolTable.isDeclared("arg1"));
+		Assert.assertFalse(symbolTable.isDeclared("arg2"));
 	}
 
-	@Test
+	/**@Test
 	public void testSetLivelinessInformation() {
 		fail("Not yet implemented");
 	}
@@ -56,11 +89,15 @@ public class SymboltableTest {
 	@Test
 	public void testGetLivelinessInformation() {
 		fail("Not yet implemented");
-	}
+	}**/
 
 	@Test
 	public void testGetNextFreeTemporary() {
-		fail("Not yet implemented");
+		String temporary2 = symbolTable.getNextFreeTemporary();
+		String temporary = symbolTable.getNextFreeTemporary();
+		Assert.assertNotNull(temporary2);
+		Assert.assertNotSame(temporary2, temporary);
+		Assert.assertFalse(temporary2.equals(temporary));
 	}
 
 }
