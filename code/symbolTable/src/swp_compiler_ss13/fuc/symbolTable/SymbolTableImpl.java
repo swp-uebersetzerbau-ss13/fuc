439ed82ac6ad889a39be0bbe7360001589cee9c1
SWP-Comp-Ch3ck3r <frank.zechert@fu-berlin.de>
2013-05-18 00:39:19 +0200

    new symbol table interface implemented

--- code/symbolTable/src/swp_compiler_ss13/fuc/symbolTable/SymbolTableImpl.java
+++ code/symbolTable/src/swp_compiler_ss13/fuc/symbolTable/SymbolTableImpl.java
@@ -6,80 +6,141 @@ import swp_compiler_ss13.common.optimization.Liveliness;
 import swp_compiler_ss13.common.parser.SymbolTable;
 import swp_compiler_ss13.common.types.Type;
 
-public class SymbolTableImpl implements SymbolTable{
+public class SymbolTableImpl implements SymbolTable {
 
 	private SymbolTable parent = null;
 	private HashMap<String, Type> symbolTable;
-	private static long ext;
-	
-	
+	private HashMap<String, String> aliases;
+	private HashMap<String, Liveliness> liveliness;
+	private static long ext = 0L;
+
 	public SymbolTableImpl(SymbolTable parent) {
 		this.parent = parent;
 		this.symbolTable = new HashMap<String, Type>();
+		this.aliases = new HashMap<String, String>();
+		this.liveliness = new HashMap<String, Liveliness>();
 	}
-	
-	public SymbolTableImpl(){
+
+	public SymbolTableImpl() {
 		this.symbolTable = new HashMap<String, Type>();
+		this.aliases = new HashMap<String, String>();
+		this.liveliness = new HashMap<String, Liveliness>();
 	}
-	
+
 	@Override
 	public SymbolTable getParentSymbolTable() {
-		return parent;
+		return this.parent;
+	}
+
+	@Override
+	public SymbolTable getRootSymbolTable() {
+		if (this.parent == null) {
+			return this;
+		}
+		return this.parent.getRootSymbolTable();
 	}
 
 	@Override
 	public Boolean isDeclared(String identifier) {
-		return symbolTable.containsKey(identifier);
+		if (this.symbolTable.containsKey(identifier)) {
+			return true;
+		}
+		if (this.parent != null) {
+			return this.parent.isDeclared(identifier);
+		}
+		return false;
+	}
+
+	@Override
+	public Boolean isDeclaredInCurrentScope(String identifier) {
+		return this.symbolTable.containsKey(identifier);
 	}
 
 	@Override
 	public Type lookupType(String identifier) {
-		return symbolTable.get(identifier);
-	}
-
-	@Override
-	public void insert(String identifier, Type type) {
-		if(!isDeclared(identifier)){
-			symbolTable.put(identifier, type);
+		if (this.isDeclaredInCurrentScope(identifier)) {
+			return this.symbolTable.get(identifier);
 		}
-	}
-
-	@Override
-	public Boolean remove(String identifier) {
-		return symbolTable.remove(identifier) != null;
-	}
-
-	@Override
-	public void setLivelinessInformation(String identifier,
-			Liveliness liveliness) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Liveliness getLivelinessInformation(String identifier) {
-		// TODO Auto-generated method stub
+		if (this.parent != null) {
+			return this.parent.lookupType(identifier);
+		}
 		return null;
 	}
 
 	@Override
+	public Type lookupTypeInCurrentScope(String identifier) {
+		if (this.isDeclaredInCurrentScope(identifier)) {
+			return this.symbolTable.get(identifier);
+		}
+		return null;
+	}
+
+	@Override
+	public Boolean insert(String identifier, Type type) {
+		if (this.isDeclaredInCurrentScope(identifier)) {
+			return false;
+		}
+		this.symbolTable.put(identifier, type);
+		return true;
+	}
+
+	@Override
+	public Boolean remove(String identifier) {
+		if (this.isDeclaredInCurrentScope(identifier)) {
+			this.symbolTable.remove(identifier);
+			this.aliases.remove(identifier);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void setLivelinessInformation(String identifier, Liveliness liveliness) {
+		this.liveliness.put(identifier, liveliness);
+	}
+
+	@Override
+	public Liveliness getLivelinessInformation(String identifier) {
+		return this.liveliness.get(identifier);
+	}
+
+	@Override
 	public String getNextFreeTemporary() {
-		String temp;
-		do{
-			temp = "tmp" + ext;
-			ext++;
-		}while(isDeclared(temp));
+		String temp = "tmp" + ext;
+		ext++;
 		return temp;
 	}
 
 	@Override
 	public void putTemporary(String identifier, Type type) {
-		if(this.parent != null){
-			parent.putTemporary(identifier, type);
-		}else{
-			insert(identifier, type);
-		}
-		
+		this.getRootSymbolTable().insert(identifier, type);
 	}
 
+	@Override
+	public void setIdentifierAlias(String identifier, String alias) {
+		this.aliases.put(identifier, alias);
+	}
+
+	@Override
+	public String getIdentifierAlias(String identifier) {
+		String alias = this.aliases.get(identifier);
+		if (alias == null) {
+			if (this.isDeclared(identifier)) {
+				return identifier;
+			}
+			return null;
+		}
+		return alias;
+	}
+
+	@Override
+	public SymbolTable getDeclaringSymbolTable(String identifier) {
+		if (this.isDeclaredInCurrentScope(identifier)) {
+			return this;
+		}
+		if (this.parent != null) {
+			return this.parent.getDeclaringSymbolTable(identifier);
+		}
+		return null;
+	}
 }
