f904f91f1d8031758e72ade2756b640a5ed85fb8
fuc-thomas <benndorf.th@googlemail.com>
2013-05-18 20:28:59 +0200

    fixed newLine-bug

--- code/lexer/test/lexer/other/AdditionalSymbolTest.java
+++ code/lexer/test/lexer/other/AdditionalSymbolTest.java
@@ -62,8 +62,9 @@ public class AdditionalSymbolTest {
 	@Test
 	public void simpleTokenizingOfSymbolsTest()
 			throws UnsupportedEncodingException {
-		String simpleSymbolString = Constants.ID1 + Constants.SEMICOLON + " "
-				+ Constants.COMMENT + " " + Constants.COMMENT_EXAMPLE;
+		String simpleSymbolString = Constants.ID1 + Constants.SEMICOLON
+				+ "\n   \n  \n\n" + Constants.COMMENT + " "
+				+ Constants.COMMENT_EXAMPLE;
 
 		this.lexer.setSourceStream(new ByteArrayInputStream(simpleSymbolString
 				.getBytes("UTF-8")));
@@ -84,13 +85,13 @@ public class AdditionalSymbolTest {
 		assertEquals(Constants.COMMENT + " " + Constants.COMMENT_EXAMPLE,
 				token.getValue());
 		assertEquals(TokenType.COMMENT, token.getTokenType());
-		assertEquals(1, token.getLine().intValue());
-		assertEquals(5, token.getColumn().intValue());
+		assertEquals(5, token.getLine().intValue());
+		assertEquals(1, token.getColumn().intValue());
 
 		token = this.lexer.getNextToken();
 		assertEquals("", token.getValue());
 		assertEquals(TokenType.EOF, token.getTokenType());
-		assertEquals(2, token.getLine().intValue());
+		assertEquals(6, token.getLine().intValue());
 		assertEquals(1, token.getColumn().intValue());
 	}
 }
