b350bc6c8fcb30035cfb3d4d7e76d847e9d0c83e
Moritz Maxeiner <moritz@ucworks.org>
2013-05-31 18:19:25 +0200

    Move LLVM IR preamble to preamble.ll

--- code/backend/src/swp_compiler_ss13/fuc/backend/LLVMBackend.java
+++ code/backend/src/swp_compiler_ss13/fuc/backend/LLVMBackend.java
@@ -9,6 +9,9 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,7 +22,24 @@ import java.util.Map;
  */
 public class LLVMBackend implements Backend
 {
+	public final String llvm_preamble;
 
+	public LLVMBackend() {
+		StringBuilder out = new StringBuilder();
+
+		try {
+			BufferedReader reader = new BufferedReader(
+				new InputStreamReader(
+					this.getClass().getResourceAsStream("preamble.ll")));
+			String line;
+			while((line = reader.readLine()) != null) {
+				out.append(line + "\n");
+			}
+		}
+		catch(IOException e) { }
+
+		this.llvm_preamble = out.toString();
+	}
 
 	/**
 	 * This function generates LLVM IR code for
