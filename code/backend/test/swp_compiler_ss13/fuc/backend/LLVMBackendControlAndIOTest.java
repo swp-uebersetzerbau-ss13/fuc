8b84e9db20fb01d83365308bd876c49d9fb57cc9
jvf@home <web@jens-fischer.eu>
2013-06-03 17:48:07 +0200

    Add branch test

--- code/backend/test/swp_compiler_ss13/fuc/backend/LLVMBackendControlAndIOTest.java
+++ code/backend/test/swp_compiler_ss13/fuc/backend/LLVMBackendControlAndIOTest.java
@@ -6,6 +6,10 @@ import swp_compiler_ss13.common.backend.Quadruple;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+import static swp_compiler_ss13.common.backend.Quadruple.EmptyArgument;
+import static swp_compiler_ss13.common.backend.Quadruple.Operator;
+
 /**
  * Tests for LLVMBackend: Controll flow and IO
  */
@@ -14,7 +18,7 @@ public class LLVMBackendControlAndIOTest extends LLVMBackendTest {
 	@Test
 	public void generateTargetCodeTest_Return_Const() throws IOException, BackendException {
 		tac.add(new QuadrupleImpl(
-			        Quadruple.Operator.RETURN,
+			        Operator.RETURN,
 			        "#1",
 			        Quadruple.EmptyArgument,
 			        Quadruple.EmptyArgument));
@@ -26,12 +30,12 @@ public class LLVMBackendControlAndIOTest extends LLVMBackendTest {
 	@Test
 	public void generateTargetCodeTest_Return_Var() throws IOException, BackendException {
 		tac.add(new QuadrupleImpl(
-			        Quadruple.Operator.DECLARE_LONG,
+			        Operator.DECLARE_LONG,
 			        "#1",
 			        Quadruple.EmptyArgument,
 			        "res"));
 		tac.add(new QuadrupleImpl(
-			        Quadruple.Operator.RETURN,
+			        Operator.RETURN,
 			        "res",
 			        Quadruple.EmptyArgument,
 			        Quadruple.EmptyArgument));
@@ -42,4 +46,74 @@ public class LLVMBackendControlAndIOTest extends LLVMBackendTest {
 			+ "  ret i64 %res.0\n";
 		expectMain(mainFunctionCode, generateCodeAsString(tac));
 	}
+
+	@Test
+	public void generateTargetCodeTest_BranchTrue() throws IOException, BackendException {
+		tac.add(new QuadrupleImpl(Operator.DECLARE_BOOLEAN, "#TRUE", EmptyArgument, "cond"));
+		tac.add(new QuadrupleImpl(Operator.BRANCH, "true", "false", "cond"));
+		tac.add(new QuadrupleImpl(Operator.LABEL, "true", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.PRINT_STRING, "#\"true branch\"", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.BRANCH, "end", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.LABEL, "false", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.PRINT_STRING, "#\"false branch\"", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.BRANCH, "end", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.LABEL, "end", EmptyArgument, EmptyArgument));
+		String mainFunctionCode = "" +
+				"  %cond = alloca i8\n" +
+				"  store i8 1, i8* %cond\n" +
+				"  %cond.0 = load i8* %cond\n" +
+				"  %cond.0.cond = trunc i8 %cond.0 to i1\n" +
+				"  br i1 %cond.0.cond, label %true, label %false\n" +
+				"  true:\n" +
+				"  %.string_0 = alloca [12 x i8]\n" +
+				"  store [12 x i8] c\"true\\20branch\\00\", [12 x i8]* %.string_0\n" +
+				"  %.tmp.0 = getelementptr [12 x i8]* %.string_0, i64 0, i64 0\n" +
+				"  call i32 (i8*, ...)* @printf(i8* %.tmp.0)\n" +
+				"  br label %end\n" +
+				"  false:\n" +
+				"  %.string_1 = alloca [13 x i8]\n" +
+				"  store [13 x i8] c\"false\\20branch\\00\", [13 x i8]* %.string_1\n" +
+				"  %.tmp.1 = getelementptr [13 x i8]* %.string_1, i64 0, i64 0\n" +
+				"  call i32 (i8*, ...)* @printf(i8* %.tmp.1)\n" +
+				"  br label %end\n" +
+				"  end:\n" +
+				"  ret i64 0\n";
+		expectMain(mainFunctionCode, generateCodeAsString(tac));
+	}
+
+	@Test
+	public void generateTargetCodeTest_BranchFalse() throws IOException, BackendException {
+		tac.add(new QuadrupleImpl(Operator.DECLARE_BOOLEAN, "#FALSE", EmptyArgument, "cond"));
+		tac.add(new QuadrupleImpl(Operator.BRANCH, "true", "false", "cond"));
+		tac.add(new QuadrupleImpl(Operator.LABEL, "true", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.PRINT_STRING, "#\"true branch\"", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.BRANCH, "end", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.LABEL, "false", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.PRINT_STRING, "#\"false branch\"", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.BRANCH, "end", EmptyArgument, EmptyArgument));
+		tac.add(new QuadrupleImpl(Operator.LABEL, "end", EmptyArgument, EmptyArgument));
+		String mainFunctionCode = "" +
+				"  %cond = alloca i8\n" +
+				"  store i8 0, i8* %cond\n" +
+				"  %cond.0 = load i8* %cond\n" +
+				"  %cond.0.cond = trunc i8 %cond.0 to i1\n" +
+				"  br i1 %cond.0.cond, label %true, label %false\n" +
+				"  true:\n" +
+				"  %.string_0 = alloca [12 x i8]\n" +
+				"  store [12 x i8] c\"true\\20branch\\00\", [12 x i8]* %.string_0\n" +
+				"  %.tmp.0 = getelementptr [12 x i8]* %.string_0, i64 0, i64 0\n" +
+				"  call i32 (i8*, ...)* @printf(i8* %.tmp.0)\n" +
+				"  br label %end\n" +
+				"  false:\n" +
+				"  %.string_1 = alloca [13 x i8]\n" +
+				"  store [13 x i8] c\"false\\20branch\\00\", [13 x i8]* %.string_1\n" +
+				"  %.tmp.1 = getelementptr [13 x i8]* %.string_1, i64 0, i64 0\n" +
+				"  call i32 (i8*, ...)* @printf(i8* %.tmp.1)\n" +
+				"  br label %end\n" +
+				"  end:\n" +
+				"  ret i64 0\n";
+		expectMain(mainFunctionCode, generateCodeAsString(tac));
+	}
+
+
 }
