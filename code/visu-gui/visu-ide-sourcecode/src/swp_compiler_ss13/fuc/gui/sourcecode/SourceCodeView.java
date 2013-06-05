a5aa58fae0f6e4a868221d318af36dbd28277c13
EdwarDDay <the_brainman@web.de>
2013-06-05 09:31:48 +0200

    update sourcecode to IDE while input
    while inputting source code to SourceCodeView the written sourcecode is added to IDE

--- code/visu-gui/visu-ide-sourcecode/src/swp_compiler_ss13/fuc/gui/sourcecode/SourceCodeView.java
+++ code/visu-gui/visu-ide-sourcecode/src/swp_compiler_ss13/fuc/gui/sourcecode/SourceCodeView.java
@@ -3,17 +3,27 @@ package swp_compiler_ss13.fuc.gui.sourcecode;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import org.apache.log4j.Logger;
 
 import swp_compiler_ss13.fuc.gui.ide.mvc.Controller;
 import swp_compiler_ss13.fuc.gui.ide.mvc.IDE;
 import swp_compiler_ss13.fuc.gui.ide.mvc.Position;
 import swp_compiler_ss13.fuc.gui.ide.mvc.View;
 
+/**
+ * @author "Eduard Wolf"
+ *
+ */
 public class SourceCodeView implements View {
+
+	private static final Logger LOG = Logger.getLogger(SourceCodeView.class);
 
 	private final SourceCodeController controller;
 
@@ -48,18 +58,14 @@ public class SourceCodeView implements View {
 		return controller;
 	}
 
-	void setSourceCode(String code) {
-		sourceCodeField.setText(code);
-	}
+	void setSourceCode(final String code) {}
 
 	@Override
 	public void initComponents(IDE ide) {
-		JButton copyButton = new JButton("copy code to model");
-		copyButton.addActionListener(new CopyListener(ide));
-		ide.addButton(copyButton, getPosition(), false);
+		sourceCodeField.getDocument().addDocumentListener(new CopyListener(ide));
 	}
 
-	private class CopyListener implements ActionListener {
+	private class CopyListener implements ActionListener, DocumentListener {
 
 		private final IDE ide;
 
@@ -67,9 +73,35 @@ public class SourceCodeView implements View {
 			this.ide = ide;
 		}
 
+		private void statusChanged() {
+			LOG.trace("wrote source code");
+			SwingUtilities.invokeLater(new Runnable() {
+
+				@Override
+				public void run() {
+					ide.setSourceCode(sourceCodeField.getText());
+				}
+			});
+		}
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			ide.setSourceCode(sourceCodeField.getText());
+			statusChanged();
+		}
+
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+			statusChanged();
+		}
+
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			statusChanged();
+		}
+
+		@Override
+		public void changedUpdate(DocumentEvent e) {
+			statusChanged();
 		}
 
 	}
