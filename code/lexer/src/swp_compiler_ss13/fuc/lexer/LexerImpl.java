34ee5e356815f7e34cb482f16e034c216ea3efed
fuc-thomas <benndorf.th@googlemail.com>
2013-06-05 20:03:32 +0200

    redesigned the string handling

--- code/lexer/src/swp_compiler_ss13/fuc/lexer/LexerImpl.java
+++ code/lexer/src/swp_compiler_ss13/fuc/lexer/LexerImpl.java
@@ -4,7 +4,10 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import swp_compiler_ss13.common.lexer.BoolToken;
 import swp_compiler_ss13.common.lexer.Lexer;
+import swp_compiler_ss13.common.lexer.NumToken;
+import swp_compiler_ss13.common.lexer.RealToken;
 import swp_compiler_ss13.common.lexer.Token;
 import swp_compiler_ss13.common.lexer.TokenType;
 import swp_compiler_ss13.fuc.lexer.token.BoolTokenImpl;
@@ -19,6 +22,7 @@ import swp_compiler_ss13.fuc.lexer.token.TokenImpl;
  * 
  */
 public class LexerImpl implements Lexer {
+
 	private Token actualToken;
 	private ArrayList<String> convertedLines;
 	private String actualLineValue;
@@ -31,6 +35,10 @@ public class LexerImpl implements Lexer {
 	private boolean isSemicolon;
 	private boolean isEOF;
 
+	/**
+	 * Method sets an {@link InputStream} for the lexer and splits it line by
+	 * line into an {@link ArrayList}
+	 */
 	@Override
 	public void setSourceStream(InputStream stream) {
 
@@ -49,8 +57,7 @@ public class LexerImpl implements Lexer {
 	}
 
 	/**
-	 * Method to initialize class variables when getting a new
-	 * {@link InputStream}
+	 * Method to initialize variables when getting new input
 	 */
 	private void init() {
 
@@ -64,6 +71,18 @@ public class LexerImpl implements Lexer {
 
 	}
 
+	/**
+	 * Method read the next token from the input and returns it with his value,
+	 * type, line of code and column of code
+	 * 
+	 * @return {@link NumToken}: token has the {@link TokenType}
+	 *         <code>NUM</code><br>
+	 *         {@link RealToken}: token has the {@link TokenType}
+	 *         <code>REAL</code><br>
+	 *         {@link BoolToken}: token has the {@link TokenType}
+	 *         <code>TRUE</code> or <code>FALSE</code><br>
+	 *         {@link Token}: otherwise<br>
+	 */
 	@Override
 	public Token getNextToken() {
 
@@ -77,7 +96,6 @@ public class LexerImpl implements Lexer {
 			this.actualTokenType = TokenType.EOF;
 
 		} else if (this.isSemicolon) {
-
 			this.actualColumn = this.actualLineValue.indexOf(";",
 					this.actualColumn + this.actualTokenValue.length() - 1) + 1;
 			this.actualTokenValue = ";";
@@ -88,15 +106,20 @@ public class LexerImpl implements Lexer {
 
 			this.abstractToken();
 
-			// TODO: skip empty line
-			if (this.actualTokenValue.length() == 0) {
-
+			/*
+			 * check if actual line is empty
+			 */
+			if (this.actualLineValue.isEmpty()
+					|| this.actualLineValue.matches("\\s")) {
 				return this.getNextToken();
 
 			}
 
 		}
 
+		/*
+		 * analyse the actual type of token
+		 */
 		switch (this.actualTokenType.name()) {
 
 		case "NUM":
@@ -131,113 +154,8 @@ public class LexerImpl implements Lexer {
 	}
 
 	/**
-	 * Method to match a a {@link String} into a {@link TokenType}
-	 * 
-	 */
-	private void matchToken() {
-		if (this.actualTokenValue
-				.matches("(\\+|-)?[0-9]+((e|E)(\\+|-)?[0-9]+)?")) {
-			this.actualTokenType = TokenType.NUM;
-		} else if (this.actualTokenValue
-				.matches("(\\+|-)?[0-9]+\\.[0-9]+((e|E)(\\+|-)?[0-9]+)?")) {
-			this.actualTokenType = TokenType.REAL;
-		} else if (this.actualTokenValue
-				.matches("\\\"(?:[^\\\"\\\\]+|\\\\.)*\\\"")) {
-			/*
-			 * 
-			 */
-			this.actualTokenType = TokenType.STRING;
-			this.actualTokenValue = this.actualLineValue.substring(
-					this.actualColumn - 1,
-					this.actualLineValue.indexOf("\"", this.actualColumn) + 1);
-		} else if (this.actualTokenValue.matches(";")) {
-			this.actualTokenType = TokenType.SEMICOLON;
-		} else if (this.actualTokenValue.matches("\\(")) {
-			this.actualTokenType = TokenType.LEFT_PARAN;
-		} else if (this.actualTokenValue.matches("\\)")) {
-			this.actualTokenType = TokenType.RIGHT_PARAN;
-		} else if (this.actualTokenValue.matches("\\{")) {
-			this.actualTokenType = TokenType.LEFT_BRACE;
-		} else if (this.actualTokenValue.matches("\\}")) {
-			this.actualTokenType = TokenType.RIGHT_BRACE;
-		} else if (this.actualTokenValue.matches("\\[")) {
-			this.actualTokenType = TokenType.LEFT_BRACKET;
-		} else if (this.actualTokenValue.matches("\\]")) {
-			this.actualTokenType = TokenType.RIGHT_BRACKET;
-		} else if (this.actualTokenValue.matches("=")) {
-			this.actualTokenType = TokenType.ASSIGNOP;
-		} else if (this.actualTokenValue.matches("&&")) {
-			this.actualTokenType = TokenType.AND;
-		} else if (this.actualTokenValue.matches("\\|\\|")) {
-			this.actualTokenType = TokenType.OR;
-		} else if (this.actualTokenValue.matches("==")) {
-			this.actualTokenType = TokenType.EQUALS;
-		} else if (this.actualTokenValue.matches("!=")) {
-			this.actualTokenType = TokenType.NOT_EQUALS;
-		} else if (this.actualTokenValue.matches("<")) {
-			this.actualTokenType = TokenType.LESS;
-		} else if (this.actualTokenValue.matches("<=")) {
-			this.actualTokenType = TokenType.LESS_OR_EQUAL;
-		} else if (this.actualTokenValue.matches(">")) {
-			this.actualTokenType = TokenType.GREATER;
-		} else if (this.actualTokenValue.matches(">=")) {
-			this.actualTokenType = TokenType.GREATER_EQUAL;
-		} else if (this.actualTokenValue.matches("\\+")) {
-			this.actualTokenType = TokenType.PLUS;
-		} else if (this.actualTokenValue.matches("\\-")) {
-			this.actualTokenType = TokenType.MINUS;
-		} else if (this.actualTokenValue.matches("\\*")) {
-			this.actualTokenType = TokenType.TIMES;
-		} else if (this.actualTokenValue.matches("\\/")) {
-			this.actualTokenType = TokenType.DIVIDE;
-		} else if (this.actualTokenValue.matches("!")) {
-			this.actualTokenType = TokenType.NOT;
-		} else if (this.actualTokenValue.matches("true")) {
-			this.actualTokenType = TokenType.TRUE;
-		} else if (this.actualTokenValue.matches("false")) {
-			this.actualTokenType = TokenType.FALSE;
-		} else if (this.actualTokenValue.matches("if")) {
-			this.actualTokenType = TokenType.IF;
-		} else if (this.actualTokenValue.matches("while")) {
-			this.actualTokenType = TokenType.WHILE;
-		} else if (this.actualTokenValue.matches("do")) {
-			this.actualTokenType = TokenType.DO;
-		} else if (this.actualTokenValue.matches("break")) {
-			this.actualTokenType = TokenType.BREAK;
-		} else if (this.actualTokenValue.matches("return")) {
-			this.actualTokenType = TokenType.RETURN;
-		} else if (this.actualTokenValue.matches("print")) {
-			this.actualTokenType = TokenType.PRINT;
-		} else if (this.actualTokenValue.matches("long")) {
-			this.actualTokenType = TokenType.LONG_SYMBOL;
-		} else if (this.actualTokenValue.matches("double")) {
-			this.actualTokenType = TokenType.DOUBLE_SYMBOL;
-		} else if (this.actualTokenValue.matches("bool")) {
-			this.actualTokenType = TokenType.BOOL_SYMBOL;
-		} else if (this.actualTokenValue.matches("string")) {
-			this.actualTokenType = TokenType.STRING_SYMBOL;
-		} else if (this.actualTokenValue.matches("[a-zA-Z]\\w*")) {
-			this.actualTokenType = TokenType.ID;
-		} else if (this.actualTokenValue.matches("#.*")) {
-			/*
-			 * when token is a comment, everything in the line after the '#' is
-			 * the value
-			 */
-			this.actualTokenType = TokenType.COMMENT;
-			this.actualTokenValue = this.actualLineValue
-					.substring(this.actualLineValue.indexOf("#"));
-			this.isNextLine = true;
-		} else if (this.actualTokenValue == "$") {
-			this.actualTokenType = TokenType.EOF;
-		} else {
-			this.actualTokenType = TokenType.NOT_A_TOKEN;
-		}
-	}
-
-	/**
 	 * Method to get the value, the actual line and the actual column of the
 	 * next token
-	 * 
 	 */
 	private void abstractToken() {
 
@@ -288,11 +206,80 @@ public class LexerImpl implements Lexer {
 				this.actualColumn = this.actualLineValue.indexOf(
 						this.actualTokenValue, this.actualColumn + 1) + 1;
 
-				// FIXME: set index to the end of the token?
 			} else {
 
 				this.actualColumn = this.actualLineValue.indexOf(
 						this.actualTokenValue, 0) + 1;
+
+			}
+
+			/*
+			 * when actual token value starts with an apostrophe everything till
+			 * the next apostrophe is the value
+			 */
+			if (this.actualTokenValue.startsWith("\"")) {
+
+				int indexOfNextApostrophe = this.actualLineValue.indexOf("\"",
+						this.actualColumn);
+
+				/*
+				 * check if string is finished in this line
+				 */
+				if (indexOfNextApostrophe == -1) {
+
+					// FIXME: try to find the apostrophe in the next line
+					System.err.println("not yet implemented");
+
+				} else {
+
+					/*
+					 * loop as long as the next unescaped apostrophe is found
+					 */
+					while (!this.checkEscapeStatus(indexOfNextApostrophe)) {
+
+						indexOfNextApostrophe = this.actualLineValue.indexOf(
+								"\"", indexOfNextApostrophe + 1);
+
+					}
+
+					/*
+					 * set the correct value for the string token
+					 */
+					this.actualTokenValue = this.actualLineValue.substring(
+							this.actualColumn - 1, indexOfNextApostrophe + 1);
+
+					/*
+					 * check if the next character after the string is a
+					 * semicolon
+					 */
+					if (this.actualLineValue.substring(
+							indexOfNextApostrophe + 1).equals(";")) {
+
+						this.isSemicolon = true;
+					}
+
+					/*
+					 * calculate the new count of token FIXME: work without
+					 * count of token or start subsequence from behind
+					 */
+					if (this.actualTokenValue.contains(" ")) {
+
+						for (int i = 0; i < this.actualLineValue.split("\\s+").length; i++) {
+
+							if (this.actualLineValue.split("\\s+")[i]
+									.contains(this.actualTokenValue
+											.subSequence(this.actualTokenValue
+													.indexOf(" ") + 1,
+													indexOfNextApostrophe + 1))) {
+
+								this.actualCountOfTokenInLine = i;
+							}
+
+						}
+
+					}
+
+				}
 
 			}
 
@@ -319,26 +306,42 @@ public class LexerImpl implements Lexer {
 	}
 
 	/**
-	 * Method checks if all tokens in line were read or the actual line is empty
-	 * and increases the line of code
+	 * Method checks if in the actual line a character is escaped or not
 	 * 
-	 * @return <code>true</code>: line must be increased <br>
-	 *         <code>else</code>: otherwise
+	 * @param index
+	 *            of the character which is probably escaped
+	 * @return <code>true</code>: character is escaped<br>
+	 *         <code>false</code>: character is not escaped
+	 */
+	private boolean checkEscapeStatus(int index) {
+		return !String.valueOf(this.actualLineValue.charAt(index - 1)).equals(
+				"\\");
+	}
+
+	/**
+	 * Method checks if the actual line is empty or all tokens in line were read
+	 * 
+	 * @return <code>true</code>: all tokens were read, line must be increased <br>
+	 *         <code>false</code>: otherwise
 	 */
 	private boolean checkLineOfCode() {
+
+		boolean increaseLine = false;
+
 		if (this.actualLineValue != null) {
+
 			int actualTokensInLine = this.actualLineValue.split("\\s+").length;
+
 			if ((this.actualLineValue.startsWith(" ") && actualTokensInLine <= this.actualCountOfTokenInLine + 1)
-					|| (!this.actualLineValue.startsWith(" ") && actualTokensInLine <= this.actualCountOfTokenInLine)
-					|| this.actualLineValue.isEmpty()
-					|| this.actualLineValue.matches("\\s")) {
-				return true;
-			} else {
-				return false;
+					|| (!this.actualLineValue.startsWith(" ") && actualTokensInLine <= this.actualCountOfTokenInLine)) {
+
+				increaseLine = true;
+
 			}
-		} else {
-			return false;
+
 		}
+
+		return increaseLine;
 	}
 
 	/**
@@ -352,4 +355,186 @@ public class LexerImpl implements Lexer {
 		this.actualCountOfTokenInLine = 0;
 
 	}
+
+	/**
+	 * Method matches the actual token value and find the type of the token
+	 * 
+	 * @see TokenType
+	 */
+	private void matchToken() {
+
+		if (this.actualTokenValue
+				.matches("(\\+|-)?[0-9]+((e|E)(\\+|-)?[0-9]+)?")) {
+
+			this.actualTokenType = TokenType.NUM;
+
+		} else if (this.actualTokenValue
+				.matches("(\\+|-)?[0-9]+\\.[0-9]+((e|E)(\\+|-)?[0-9]+)?")) {
+
+			this.actualTokenType = TokenType.REAL;
+
+		} else if (this.actualTokenValue
+				.matches("\\\"(?:[^\\\"\\\\]+|\\\\.)*\\\"")) {
+
+			this.actualTokenType = TokenType.STRING;
+
+		} else if (this.actualTokenValue.matches(";")) {
+
+			this.actualTokenType = TokenType.SEMICOLON;
+
+		} else if (this.actualTokenValue.matches("\\(")) {
+
+			this.actualTokenType = TokenType.LEFT_PARAN;
+
+		} else if (this.actualTokenValue.matches("\\)")) {
+
+			this.actualTokenType = TokenType.RIGHT_PARAN;
+
+		} else if (this.actualTokenValue.matches("\\{")) {
+
+			this.actualTokenType = TokenType.LEFT_BRACE;
+
+		} else if (this.actualTokenValue.matches("\\}")) {
+
+			this.actualTokenType = TokenType.RIGHT_BRACE;
+
+		} else if (this.actualTokenValue.matches("\\[")) {
+
+			this.actualTokenType = TokenType.LEFT_BRACKET;
+
+		} else if (this.actualTokenValue.matches("\\]")) {
+
+			this.actualTokenType = TokenType.RIGHT_BRACKET;
+
+		} else if (this.actualTokenValue.matches("=")) {
+
+			this.actualTokenType = TokenType.ASSIGNOP;
+
+		} else if (this.actualTokenValue.matches("&&")) {
+
+			this.actualTokenType = TokenType.AND;
+
+		} else if (this.actualTokenValue.matches("\\|\\|")) {
+
+			this.actualTokenType = TokenType.OR;
+
+		} else if (this.actualTokenValue.matches("==")) {
+
+			this.actualTokenType = TokenType.EQUALS;
+
+		} else if (this.actualTokenValue.matches("!=")) {
+
+			this.actualTokenType = TokenType.NOT_EQUALS;
+
+		} else if (this.actualTokenValue.matches("<")) {
+
+			this.actualTokenType = TokenType.LESS;
+
+		} else if (this.actualTokenValue.matches("<=")) {
+
+			this.actualTokenType = TokenType.LESS_OR_EQUAL;
+
+		} else if (this.actualTokenValue.matches(">")) {
+
+			this.actualTokenType = TokenType.GREATER;
+
+		} else if (this.actualTokenValue.matches(">=")) {
+
+			this.actualTokenType = TokenType.GREATER_EQUAL;
+
+		} else if (this.actualTokenValue.matches("\\+")) {
+
+			this.actualTokenType = TokenType.PLUS;
+
+		} else if (this.actualTokenValue.matches("\\-")) {
+
+			this.actualTokenType = TokenType.MINUS;
+
+		} else if (this.actualTokenValue.matches("\\*")) {
+
+			this.actualTokenType = TokenType.TIMES;
+
+		} else if (this.actualTokenValue.matches("\\/")) {
+
+			this.actualTokenType = TokenType.DIVIDE;
+
+		} else if (this.actualTokenValue.matches("!")) {
+
+			this.actualTokenType = TokenType.NOT;
+
+		} else if (this.actualTokenValue.matches("true")) {
+
+			this.actualTokenType = TokenType.TRUE;
+
+		} else if (this.actualTokenValue.matches("false")) {
+
+			this.actualTokenType = TokenType.FALSE;
+
+		} else if (this.actualTokenValue.matches("if")) {
+
+			this.actualTokenType = TokenType.IF;
+
+		} else if (this.actualTokenValue.matches("while")) {
+
+			this.actualTokenType = TokenType.WHILE;
+
+		} else if (this.actualTokenValue.matches("do")) {
+
+			this.actualTokenType = TokenType.DO;
+
+		} else if (this.actualTokenValue.matches("break")) {
+
+			this.actualTokenType = TokenType.BREAK;
+
+		} else if (this.actualTokenValue.matches("return")) {
+
+			this.actualTokenType = TokenType.RETURN;
+
+		} else if (this.actualTokenValue.matches("print")) {
+
+			this.actualTokenType = TokenType.PRINT;
+
+		} else if (this.actualTokenValue.matches("long")) {
+
+			this.actualTokenType = TokenType.LONG_SYMBOL;
+
+		} else if (this.actualTokenValue.matches("double")) {
+
+			this.actualTokenType = TokenType.DOUBLE_SYMBOL;
+
+		} else if (this.actualTokenValue.matches("bool")) {
+
+			this.actualTokenType = TokenType.BOOL_SYMBOL;
+
+		} else if (this.actualTokenValue.matches("string")) {
+
+			this.actualTokenType = TokenType.STRING_SYMBOL;
+
+		} else if (this.actualTokenValue.matches("[a-zA-Z]\\w*")) {
+
+			this.actualTokenType = TokenType.ID;
+
+		} else if (this.actualTokenValue.matches("#.*")) {
+
+			/*
+			 * when token is a comment, everything in the line after the '#' is
+			 * part of the value
+			 */
+			this.actualTokenType = TokenType.COMMENT;
+			this.actualTokenValue = this.actualLineValue
+					.substring(this.actualLineValue.indexOf("#"));
+			this.isNextLine = true;
+
+		} else if (this.actualTokenValue == "$") {
+
+			this.actualTokenType = TokenType.EOF;
+
+		} else {
+
+			this.actualTokenType = TokenType.NOT_A_TOKEN;
+
+		}
+
+	}
+
 }
