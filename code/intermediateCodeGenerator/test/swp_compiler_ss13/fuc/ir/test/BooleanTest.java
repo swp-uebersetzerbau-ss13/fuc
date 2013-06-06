5780ae9ba260a5eb07909c58048e1f31dde9b986
SWP-Comp-Deathk <danny.maasch@fu-berlin.de>
2013-06-06 09:47:50 +0200

    More tests(better coverage)

--- code/intermediateCodeGenerator/test/swp_compiler_ss13/fuc/ir/test/BooleanTest.java
+++ code/intermediateCodeGenerator/test/swp_compiler_ss13/fuc/ir/test/BooleanTest.java
@@ -11,12 +11,12 @@ import swp_compiler_ss13.common.ast.AST;
 import swp_compiler_ss13.common.ast.nodes.binary.BinaryExpressionNode.BinaryOperator;
 import swp_compiler_ss13.common.backend.Quadruple;
 import swp_compiler_ss13.common.ir.IntermediateCodeGeneratorException;
-import swp_compiler_ss13.common.types.primitive.DoubleType;
+import swp_compiler_ss13.common.types.primitive.BooleanType;
 import swp_compiler_ss13.fuc.ast.ASTFactory;
 import swp_compiler_ss13.fuc.ir.IntermediateCodeGeneratorImpl;
 import swp_compiler_ss13.fuc.symbolTable.SymbolTableImpl;
 
-public class DoubleOperatorTest {
+public class BooleanTest {
 
 	private AST ast;
 
@@ -24,42 +24,25 @@ public class DoubleOperatorTest {
 	public void setUp() throws Exception {
 		PA.setValue(SymbolTableImpl.class, "ext", 0);
 		ASTFactory astf = new ASTFactory();
-		astf.addDeclaration("a", new DoubleType());
-		astf.addDeclaration("b", new DoubleType());
-		astf.addDeclaration("c", new DoubleType());
-
-		astf.addAssignment(astf.newBasicIdentifier("a"),
-				astf.newLiteral("4", new DoubleType()));
+		astf.addDeclaration("b", new BooleanType());
+		astf.addDeclaration("c", new BooleanType());
 
 		astf.addAssignment(astf.newBasicIdentifier("b"),
-				astf.newLiteral("3", new DoubleType()));
-
+				astf.newLiteral("true", new BooleanType()));
 		astf.addAssignment(astf.newBasicIdentifier("c"),
-				astf.newLiteral("2", new DoubleType()));
+				astf.newLiteral("false", new BooleanType()));
 
 		astf.addAssignment(
-				astf.newBasicIdentifier("c"),
-				astf.newBinaryExpression(
-						BinaryOperator.MULTIPLICATION,
-						astf.newBinaryExpression(BinaryOperator.DIVISION,
-								astf.newBasicIdentifier("a"),
-								astf.newBasicIdentifier("b")),
+				astf.newBasicIdentifier("b"),
+				astf.newBinaryExpression(BinaryOperator.LOGICAL_AND,
+						astf.newBasicIdentifier("b"),
 						astf.newBasicIdentifier("c")));
-		astf.addReturn(astf.newBasicIdentifier("c"));
-
-		astf.addAssignment(astf.newBasicIdentifier("c"), astf
-				.newBinaryExpression(BinaryOperator.ADDITION, astf
-						.newBinaryExpression(BinaryOperator.SUBSTRACTION,
-								astf.newBasicIdentifier("a"),
-								astf.newBasicIdentifier("b")), astf
-						.newBasicIdentifier("c")));
-		astf.addReturn(astf.newBasicIdentifier("c"));
 
 		ast = astf.getAST();
 	}
 
 	@Test
-	public void test() throws IntermediateCodeGeneratorException {
+	public void testboolean() throws IntermediateCodeGeneratorException {
 		IntermediateCodeGeneratorImpl irg = new IntermediateCodeGeneratorImpl();
 		List<Quadruple> irc = irg.generateIntermediateCode(ast);
 
