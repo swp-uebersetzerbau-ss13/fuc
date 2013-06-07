796b1529ba2b6145b17c49a1fb6356c1b983a8c9
dong <dong@dong_laptop>
2013-06-07 16:48:56 +0200

    -add long and double in GrammarTestHelper.java -add Testtoken in M2PrintTest.java

--- code/parser/test/swp_compiler_ss13/fuc/parser/GrammarTestHelper.java
+++ code/parser/test/swp_compiler_ss13/fuc/parser/GrammarTestHelper.java
@@ -2,6 +2,7 @@ package swp_compiler_ss13.fuc.parser;
 
 import swp_compiler_ss13.common.lexer.Token;
 import swp_compiler_ss13.common.lexer.TokenType;
+import swp_compiler_ss13.fuc.parser.TestToken;
 import swp_compiler_ss13.fuc.parser.grammar.Terminal;
 
 public class GrammarTestHelper {
@@ -10,6 +11,14 @@ public class GrammarTestHelper {
 	public static Token num(int i) {
 		return new TestToken(i + "", TokenType.NUM);
 	}
+	// for long
+		public static Token longe(long  i) {
+			return new TestToken(i + "", TokenType.LONG_SYMBOL);
+		}
+		// for  double
+			public static Token doublee(double i) {
+				return new TestToken(i + "", TokenType.DOUBLE_SYMBOL);
+			}
 
 	public static Token t(Terminal terminal) {
 		// TODO Handle special terminals better
