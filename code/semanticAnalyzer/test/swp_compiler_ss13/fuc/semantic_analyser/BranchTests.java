3fc31d5ec4df9f5ecc5b8a24224f12c4ceac7889
swpcomp-heydu <swpcomp-heydu@swp-uebersetzerbau-ss13>
2013-06-09 21:03:49 +0200

    Semantic Analyser: Initialization test for if/else

--- code/semanticAnalyzer/test/swp_compiler_ss13/fuc/semantic_analyser/BranchTests.java
+++ code/semanticAnalyzer/test/swp_compiler_ss13/fuc/semantic_analyser/BranchTests.java
@@ -13,7 +13,9 @@ import swp_compiler_ss13.common.ast.nodes.leaf.LiteralNode;
 import swp_compiler_ss13.common.ast.nodes.marynary.BlockNode;
 import swp_compiler_ss13.common.ast.nodes.ternary.BranchNode;
 import swp_compiler_ss13.common.ast.nodes.unary.DeclarationNode;
+import swp_compiler_ss13.common.ast.nodes.unary.ReturnNode;
 import swp_compiler_ss13.common.parser.SymbolTable;
+import swp_compiler_ss13.common.types.primitive.BooleanType;
 import swp_compiler_ss13.common.types.primitive.LongType;
 import swp_compiler_ss13.fuc.ast.ASTImpl;
 import swp_compiler_ss13.fuc.ast.AssignmentNodeImpl;
@@ -22,6 +24,7 @@ import swp_compiler_ss13.fuc.ast.BlockNodeImpl;
 import swp_compiler_ss13.fuc.ast.BranchNodeImpl;
 import swp_compiler_ss13.fuc.ast.DeclarationNodeImpl;
 import swp_compiler_ss13.fuc.ast.LiteralNodeImpl;
+import swp_compiler_ss13.fuc.ast.ReturnNodeImpl;
 import swp_compiler_ss13.fuc.errorLog.ReportLogImpl;
 import swp_compiler_ss13.fuc.symbolTable.SymbolTableImpl;
 
@@ -120,5 +123,174 @@ public class BranchTests {
 		assertEquals(log.getErrors().size(), 1);
 	}
 
+	/**
+	 * long l;<br/>
+	 * bool b;<br/>
+	 * <br/>
+	 * if (b)<br/>
+	 *     l = 0;<br/>
+	 * return l;
+	 */
+	@Test
+	public void initilizationOnlyInOneBranchTest() {
+		// long l;
+		DeclarationNode declaration_l = new DeclarationNodeImpl();
+		declaration_l.setIdentifier("l");
+		declaration_l.setType(new LongType());
+		
+		// bool b;
+		DeclarationNode declaration_b = new DeclarationNodeImpl();
+		declaration_b.setIdentifier("b");
+		declaration_b.setType(new BooleanType());
+		
+		// l = 0;
+		BasicIdentifierNode identifier_l2 = new BasicIdentifierNodeImpl();
+		identifier_l2.setIdentifier("l");
+		LiteralNode literal_0 = new LiteralNodeImpl();
+		literal_0.setLiteral("0");
+		literal_0.setLiteralType(new LongType());
+		
+		AssignmentNode assignment_l2 = new AssignmentNodeImpl();
+		assignment_l2.setLeftValue(identifier_l2);
+		assignment_l2.setRightValue(literal_0);
+		identifier_l2.setParentNode(assignment_l2);
+		literal_0.setParentNode(assignment_l2);
+		
+		// if (b) ...;
+		BasicIdentifierNode identifier_l3 = new BasicIdentifierNodeImpl();
+		identifier_l3.setIdentifier("b");
+		
+		BranchNode branch = new BranchNodeImpl();
+		branch.setCondition(identifier_l3);
+		branch.setStatementNodeOnTrue(assignment_l2);
+		identifier_l3.setParentNode(branch);
+		assignment_l2.setParentNode(branch);
+		
+		// return l;
+
+		BasicIdentifierNode identifier = new BasicIdentifierNodeImpl();
+		identifier.setIdentifier("l");
+		
+		ReturnNode returnNode = new ReturnNodeImpl();
+		returnNode.setRightValue(identifier);
+		identifier.setParentNode(returnNode);
+		
+		// main block
+		SymbolTable symbolTable = new SymbolTableImpl();
+		symbolTable.insert("l", new LongType());
+		symbolTable.insert("b", new BooleanType());
+		
+		BlockNode blockNode = new BlockNodeImpl();
+		blockNode.addDeclaration(declaration_l);
+		blockNode.addStatement(branch);
+		blockNode.addStatement(returnNode);
+		blockNode.setSymbolTable(symbolTable);
+		declaration_l.setParentNode(blockNode);
+		branch.setParentNode(blockNode);
+		
+		// analyse AST
+		AST ast = new ASTImpl();
+		ast.setRootNode(blockNode);
+		
+		analyser.analyse(ast);
+		
+		// TODO better error-check
+		System.out.println(log);
+		assertEquals(log.hasErrors(), false);
+		assertEquals(log.hasWarnings(), true);
+	}
+
+	/**
+	 * long l;<br/>
+	 * bool b;<br/>
+	 * <br/>
+	 * if (b)<br/>
+	 *     l = 0;<br/>
+	 * else
+	 *     l = 1;
+	 * return l;
+	 */
+	@Test
+	public void initilizationInBothBranchTest() {
+		// long l;
+		DeclarationNode declaration_l = new DeclarationNodeImpl();
+		declaration_l.setIdentifier("l");
+		declaration_l.setType(new LongType());
+		
+		// bool b;
+		DeclarationNode declaration_b = new DeclarationNodeImpl();
+		declaration_b.setIdentifier("b");
+		declaration_b.setType(new BooleanType());
+		
+		// l = 0;
+		BasicIdentifierNode identifier_l2 = new BasicIdentifierNodeImpl();
+		identifier_l2.setIdentifier("l");
+		LiteralNode literal_0 = new LiteralNodeImpl();
+		literal_0.setLiteral("0");
+		literal_0.setLiteralType(new LongType());
+		
+		AssignmentNode assignment_l2 = new AssignmentNodeImpl();
+		assignment_l2.setLeftValue(identifier_l2);
+		assignment_l2.setRightValue(literal_0);
+		identifier_l2.setParentNode(assignment_l2);
+		literal_0.setParentNode(assignment_l2);
+		
+		// l = 1;
+		BasicIdentifierNode identifier_l21 = new BasicIdentifierNodeImpl();
+		identifier_l21.setIdentifier("l");
+		LiteralNode literal_1 = new LiteralNodeImpl();
+		literal_1.setLiteral("1");
+		literal_1.setLiteralType(new LongType());
+		
+		AssignmentNode assignment_l21 = new AssignmentNodeImpl();
+		assignment_l21.setLeftValue(identifier_l21);
+		assignment_l21.setRightValue(literal_1);
+		identifier_l21.setParentNode(assignment_l21);
+		literal_1.setParentNode(assignment_l21);
+		
+		// if (b) ...;
+		BasicIdentifierNode identifier_l3 = new BasicIdentifierNodeImpl();
+		identifier_l3.setIdentifier("b");
+		
+		BranchNode branch = new BranchNodeImpl();
+		branch.setCondition(identifier_l3);
+		branch.setStatementNodeOnTrue(assignment_l2);
+		branch.setStatementNodeOnFalse(assignment_l21);
+		identifier_l3.setParentNode(branch);
+		assignment_l2.setParentNode(branch);
+		
+		// return l;
+
+		BasicIdentifierNode identifier = new BasicIdentifierNodeImpl();
+		identifier.setIdentifier("l");
+		
+		ReturnNode returnNode = new ReturnNodeImpl();
+		returnNode.setRightValue(identifier);
+		identifier.setParentNode(returnNode);
+		
+		// main block
+		SymbolTable symbolTable = new SymbolTableImpl();
+		symbolTable.insert("l", new LongType());
+		symbolTable.insert("b", new BooleanType());
+		
+		BlockNode blockNode = new BlockNodeImpl();
+		blockNode.addDeclaration(declaration_l);
+		blockNode.addStatement(branch);
+		blockNode.addStatement(returnNode);
+		blockNode.setSymbolTable(symbolTable);
+		declaration_l.setParentNode(blockNode);
+		branch.setParentNode(blockNode);
+		
+		// analyse AST
+		AST ast = new ASTImpl();
+		ast.setRootNode(blockNode);
+		
+		analyser.analyse(ast);
+		
+		// TODO better error-check
+		System.out.println(log);
+		assertEquals(log.hasErrors(), false);
+		assertEquals(log.getEntries().size(), 1);
+	}
 
 }
