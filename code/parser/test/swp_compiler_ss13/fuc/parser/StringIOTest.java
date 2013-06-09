c38d2189b8d4b94ab10c0b9090eb58d7e17f8b45
gero <geroleinemann@gmx.de>
2013-06-09 22:36:36 +0200

    Added LineBreak testcases

--- code/parser/test/swp_compiler_ss13/fuc/parser/StringIOTest.java
+++ code/parser/test/swp_compiler_ss13/fuc/parser/StringIOTest.java
@@ -7,8 +7,11 @@ import java.io.ByteArrayInputStream;
 import org.junit.Test;
 
 import swp_compiler_ss13.common.ast.AST;
+import swp_compiler_ss13.common.ast.nodes.ternary.BranchNode;
 import swp_compiler_ss13.common.lexer.Lexer;
 import swp_compiler_ss13.common.report.ReportLog;
+import swp_compiler_ss13.common.types.primitive.BooleanType;
+import swp_compiler_ss13.common.types.primitive.LongType;
 import swp_compiler_ss13.common.types.primitive.StringType;
 import swp_compiler_ss13.fuc.ast.ASTFactory;
 import swp_compiler_ss13.fuc.errorLog.ReportLogImpl;
@@ -28,6 +31,57 @@ public class StringIOTest {
 	public void testLinebreakInString() {
 		String input = "string s;\ns = \"test1\\ntest2\";\nreturn;";
 		
+		// Parse
+		AST ast = parseToAst(input);
+		
+		// Construct expected AST
+		ASTFactory factory = new ASTFactory();
+		factory.addDeclaration("s", new StringType(STRING_LENGTH));
+		factory.addAssignment(factory.newBasicIdentifier("s"), factory.newLiteral("\"test1\\ntest2\"", new StringType(14L)));
+		factory.addReturn(null);
+		
+		AST expected = factory.getAST();
+		ASTComparator.compareAST(expected, ast);
+	}
+	
+	@Test
+	public void testConditional() {
+		String input = "bool b;\nlong l;\nif ( b ) l = 1;\nreturn l;";
+		
+		// Parse
+		AST ast = parseToAst(input);
+		
+		// Construct expected AST
+		ASTFactory factory = new ASTFactory();
+		factory.addDeclaration("b", new BooleanType());
+		factory.addDeclaration("l", new LongType());
+		BranchNode iff = factory.addBranch(factory.newBasicIdentifier("b"));
+		iff.setStatementNodeOnTrue(factory.newAssignment(factory.newBasicIdentifier("l"), factory.newLiteral("1", new LongType())));
+		factory.goToParent();
+		factory.addReturn(factory.newBasicIdentifier("l"));
+		
+		AST expected = factory.getAST();
+		ASTComparator.compareAST(expected, ast);
+	}
+	
+	@Test
+	public void testHalloWelt() {
+		String input = "string s;\ns = \"Hallo\\nWelt\";return s;";
+		
+		// Parse
+		AST ast = parseToAst(input);
+		
+		// Construct expected AST
+		ASTFactory factory = new ASTFactory();
+		factory.addDeclaration("s", new StringType(LRParser.STRING_LENGTH));
+		factory.addAssignment(factory.newBasicIdentifier("s"), factory.newLiteral("\"Hallo\\nWelt\"", new StringType(13L)));
+		factory.addReturn(factory.newBasicIdentifier("s"));
+		
+		AST expected = factory.getAST();
+		ASTComparator.compareAST(expected, ast);
+	}
+	
+	private static AST parseToAst(String input) {
 		// Generate parsing table
 		Grammar grammar = new ProjectGrammar.Complete().getGrammar();
 		ALRGenerator<LR1Item, LR1State> generator = new LR1Generator(grammar);
@@ -41,15 +95,6 @@ public class StringIOTest {
 		LRParser lrParser = new LRParser();
 		LexerWrapper lexWrapper = new LexerWrapper(lexer, grammar);
 		ReportLog reportLog = new ReportLogImpl();
-		AST ast = lrParser.parse(lexWrapper, reportLog, table);
-		
-		// Construct expected AST
-		ASTFactory factory = new ASTFactory();
-		factory.addDeclaration("s", new StringType(STRING_LENGTH));
-		factory.addAssignment(factory.newBasicIdentifier("s"), factory.newLiteral("\"test1\\ntest2\"", new StringType(14L)));
-		factory.addReturn(null);
-		
-		AST expected = factory.getAST();
-		ASTComparator.compareAST(expected, ast);
+		return lrParser.parse(lexWrapper, reportLog, table);
 	}
 }
