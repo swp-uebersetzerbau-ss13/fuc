cd121a165ac427b99eafd6975571605614a0f45d
kensan <bgoverlord@gmail.com>
2013-06-13 15:52:22 +0200

    test for Arrays added

--- code/parser/test/swp_compiler_ss13/fuc/parser/ArrayTest.java
+++ code/parser/test/swp_compiler_ss13/fuc/parser/ArrayTest.java
@@ -1,20 +1,18 @@
 package swp_compiler_ss13.fuc.parser;
 
 import static org.junit.Assert.assertNotNull;
-import static swp_compiler_ss13.fuc.parser.GrammarTestHelper.id;
 
 import java.io.ByteArrayInputStream;
 
 import org.apache.log4j.BasicConfigurator;
 import org.junit.Test;
 
+import swp_compiler_ss13.fuc.parser.errorHandling.ParserASTXMLVisualization;
 import swp_compiler_ss13.common.ast.AST;
 import swp_compiler_ss13.common.lexer.Lexer;
-import swp_compiler_ss13.common.lexer.TokenType;
 import swp_compiler_ss13.common.report.ReportLog;
 import swp_compiler_ss13.fuc.errorLog.ReportLogImpl;
 import swp_compiler_ss13.fuc.lexer.LexerImpl;
-import swp_compiler_ss13.fuc.lexer.token.NumTokenImpl;
 import swp_compiler_ss13.fuc.parser.generator.ALRGenerator;
 import swp_compiler_ss13.fuc.parser.generator.LR1Generator;
 import swp_compiler_ss13.fuc.parser.generator.items.LR1Item;
@@ -30,30 +28,13 @@ public class ArrayTest {
 		BasicConfigurator.configure();
 	}
 
-	public void testArray() {
-		// Generate parsing table
-		Grammar grammar = new ProjectGrammar.Complete().getGrammar();
-		ALRGenerator<LR1Item, LR1State> generator = new LR1Generator(grammar);
-		LRParsingTable table = generator.getParsingTable();
 
-		// Simulate input
-		Lexer lexer = new TestLexer(
-				new TestToken("bool", TokenType.BOOL_SYMBOL), new TestToken("[", TokenType.LEFT_BRACKET),
-				new NumTokenImpl("3",TokenType.NUM,0,0), new TestToken("]",TokenType.RIGHT_BRACKET), id("b"));
-
-		// Run LR-parser with table
-		LRParser lrParser = new LRParser();
-		
-		LexerWrapper lexWrapper = new LexerWrapper(lexer, grammar);
-		ReportLog reportLog = new ReportLogImpl();
-		AST ast = lrParser.parse(lexWrapper, reportLog, table);
-
-//		checkAst(ast);
-	}
 
 	@Test
 	public void testCondOrgLexer() throws Exception {
-		String input = "bool [ 5 ] b;\n";
+		String input = "bool [ 5 ] b;\n" +
+						"bool a;" + 
+						"a = b [ 2 ] ;";
 		
 		// Generate parsing table
 		Grammar grammar = new ProjectGrammar.Complete().getGrammar();
@@ -74,6 +55,7 @@ public class ArrayTest {
 
 	private static void checkAst(AST ast) {
 		assertNotNull(ast);
+		System.out.println(new ParserASTXMLVisualization().visualizeAST(ast));
 
 //		// Create reference AST
 //		ASTFactory factory = new ASTFactory();
