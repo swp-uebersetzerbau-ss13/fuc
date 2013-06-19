6b7f5d39087361036cef2b45aa634f14373cfd49
gero <geroleinemann@gmx.de>
2013-06-19 20:28:55 +0200

    Test fix...

--- code/parser/test/swp_compiler_ss13/fuc/parser/generator/ALRGeneratorTest.java
+++ code/parser/test/swp_compiler_ss13/fuc/parser/generator/ALRGeneratorTest.java
@@ -1,27 +1,44 @@
 package swp_compiler_ss13.fuc.parser.generator;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import swp_compiler_ss13.fuc.parser.generator.items.LR0Item;
+import swp_compiler_ss13.fuc.parser.generator.states.LR0State;
+import swp_compiler_ss13.fuc.parser.grammar.Grammar;
+import swp_compiler_ss13.fuc.parser.grammar.ProjectGrammar;
+
 
 public class ALRGeneratorTest {
-//	// Generate parsing table
-//	Grammar grammar = new ProjectGrammar.Complete().getGrammar();
-//	ALRGenerator<LR0Item, LR0State> generator = new LR0Generator(grammar);
-//	LRParsingTable table = generator.getParsingTable();
-//	
-//
-//	@Test
-//	public final void testCreateDFA() {
-//	assertNotNull(generator.createDFA().toString());
-//		 
-//	}
-//
-//	@Test
-//	public final void testGetGrammarInfo() {
-//		assertNotNull(generator.getGrammarInfo().toString());
-//	}
-//
-//	@Test
-//	public final void testGetParsingTable() {
-//		assertNotNull(generator.getParsingTable().toString());
-//	}
+	// Generate parsing table
+	private final Grammar grammar = new ProjectGrammar.Complete().getGrammar();
+	private final ALRGenerator<LR0Item, LR0State> generator;
+//	private final LRParsingTable table;
+	
+	public ALRGeneratorTest() {
+		try {
+			this.generator = new LR0Generator(grammar);
+		} catch (GeneratorException err) {
+			throw new RuntimeException("Error while generating LR(1) grammar");
+		}
+//		table = generator.getParsingTable();
+	}
+
+	@Test
+	public final void testCreateDFA() {
+	assertNotNull(generator.createDFA().toString());
+		 
+	}
+
+	@Test
+	public final void testGetGrammarInfo() {
+		assertNotNull(generator.getGrammarInfo().toString());
+	}
+
+	@Test
+	public final void testGetParsingTable() {
+		assertNotNull(generator.getParsingTable().toString());
+	}
 
 }
